0385365ef2b1e9d1592884eb99d707dfa2d9e0a3
Naman <namanarora11@gmail.com>
2015-04-17 12:33:55 +0530

    Stock

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 SMP PREEMPT Fri Apr 17 12:23:52 IST 2015"
+#define LINUX_COMPILE_BY "build_admin"
+#define LINUX_COMPILE_HOST "ubuntu_builder"
+#define LINUX_COMPILER "gcc version 4.9.1 20140505 (prerelease) (crosstool-NG linaro-1.13.1+bzr2663 - Linaro GCC 2014.05) "
